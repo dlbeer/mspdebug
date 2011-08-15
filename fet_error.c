3a8994b4f79dbcc87feb6c653468abd74eb1c9b0
Tamas TEVESZ <ice@extreme.hu>
2011-08-15 13:11:35 +0200

    Add more error codes from MSP430.dll v3:
    http://processors.wiki.ti.com/index.php/MSP430.dll_v3

--- fet_error.c
+++ fet_error.c
@@ -22,68 +22,81 @@
 
 #include "util.h"
 
-/* This table is taken from uif430 */
+/* These messages come from uif430 and from MSP430.DLL V3. */
 static const char *error_strings[] =
 {
-        "No error",                                                     // 0
-        "Could not initialize device interface",                        // 1
-        "Could not close device interface",                             // 2
-        "Invalid parameter(s)",                                         // 3
-        "Could not find device (or device not supported)",              // 4
-        "Unknown device",                                               // 5
-        "Could not read device memory",                                 // 6
-        "Could not write device memory",                                // 7
-        "Could not read device configuration fuses",                    // 8
-        "Incorrectly configured device; device derivative not supported",// 9
+	"No error",								// 0
+	"Could not initialize device interface",				// 1
+	"Could not close device interface",					// 2
+	"Invalid parameter(s)",							// 3
+	"Could not find device (or device not supported)",			// 4
+	"Unknown device",							// 5
+	"Could not read device memory",						// 6
+	"Could not write device memory",					// 7
+	"Could not read device configuration fuses",				// 8
+	"Incorrectly configured device; device derivative not supported",	// 9
 
-        "Could not set device Vcc",                                     // 10
-        "Could not reset device",                                       // 11
-        "Could not preserve/restore device memory",                     // 12
-        "Could not set device operating frequency",                     // 13
-        "Could not erase device memory",                                // 14
-        "Could not set device breakpoint",                              // 15
-        "Could not single step device",                                 // 16
-        "Could not run device (to breakpoint)",                         // 17
-        "Could not determine device state",                             // 18
-        "Could not open Enhanced Emulation Module",                     // 19
+	"Could not set device Vcc",						// 10
+	"Could not reset device",						// 11
+	"Could not preserve/restore device memory",				// 12
+	"Could not set device operating frequency",				// 13
+	"Could not erase device memory",					// 14
+	"Could not set device breakpoint",					// 15
+	"Could not single step device",						// 16
+	"Could not run device (to breakpoint)",					// 17
+	"Could not determine device state",					// 18
+	"Could not open Enhanced Emulation Module",				// 19
 
-        "Could not read Enhanced Emulation Module register",            // 20
-        "Could not write Enhanced Emulation Module register",           // 21
-        "Could not close Enhanced Emulation Module",                    // 22
-        "File open error",                                              // 23
-        "Could not determine file type",                                // 24
-        "Unexpected end of file encountered",                           // 25
-        "File input/output error",                                      // 26
-        "File data error",                                              // 27
-        "Verification error",                                           // 28
-        "Could not blow device security fuse",                          // 29
+	"Could not read Enhanced Emulation Module register",			// 20
+	"Could not write Enhanced Emulation Module register",			// 21
+	"Could not close Enhanced Emulation Module",				// 22
+	"File open error",							// 23
+	"Could not determine file type",					// 24
+	"Unexpected end of file encountered",					// 25
+	"File input/output error",						// 26
+	"File data error",							// 27
+	"Verification error",							// 28
+	"Could not blow device security fuse",					// 29
 
-        "Could not access device - security fuse is blown",             // 30
-        "Error within Intel Hex file",                                  // 31
-        "Could not write device Register",                              // 32
-        "Could not read device Register",                               // 33
-        "Not supported by selected Interface",                          // 34
-        "Could not communicate with FET",                               // 35
-        "No external power supply detected",                            // 36
-        "External power too low",                                       // 37
-        "External power detected",                                      // 38
-        "External power too high",                                      // 39
+	"Could not access device - security fuse is blown",			// 30
+	"Error within Intel Hex file",						// 31
+	"Could not write device Register",					// 32
+	"Could not read device Register",					// 33
+	"Not supported by selected Interface",					// 34
+	"Could not communicate with FET",					// 35
+	"No external power supply detected",					// 36
+	"External power too low",						// 37
+	"External power detected",						// 38
+	"External power too high",						// 39
 
-        "Hardware Self Test Error",                                     // 40
-        "Fast Flash Routine experienced a timeout",                     // 41
-        "Could not create thread for polling",                          // 42
-        "Could not initialize Enhanced Emulation Module",               // 43
-        "Insufficient resources",                                       // 44
-        "No clock control emulation on connected device",               // 45
-        "No state storage buffer implemented on connected device",      // 46
-        "Could not read trace buffer",                                  // 47
-        "Enable the variable watch function",                           // 48
-        "No trigger sequencer implemented on connected device",         // 49
+	"Hardware Self Test Error",						// 40
+	"Fast Flash Routine experienced a timeout",				// 41
+	"Could not create thread for polling",					// 42
+	"Could not initialize Enhanced Emulation Module",			// 43
+	"Insufficient resources",						// 44
+	"No clock control emulation on connected device",			// 45
+	"No state storage buffer implemented on connected device",		// 46
+	"Could not read trace buffer",						// 47
+	"Enable the variable watch function",					// 48
+	"No trigger sequencer implemented on connected device",			// 49
 
-        "Could not read sequencer state - Sequencer is disabled",       // 50
-        "Could not remove trigger - Used in sequencer",                 // 51
-        "Could not set combination - Trigger is used in sequencer",     // 52
-        "Invalid error number",                                         // 53
+	"Could not read sequencer state - Sequencer is disabled",		// 50
+	"Could not remove trigger - Used in sequencer",				// 51
+	"Could not set combination - Trigger is used in sequencer",		// 52
+	"System Protection Module A is enabled - Device locked",		// 53
+	"Invalid SPMA key was passed to the target device - Device locked",	// 54
+	"Device does not accept any further SPMA keys - Device locked",		// 55
+	"MSP-FET430UIF Firmware erased - Bootloader active",			// 56
+	"Could not find MSP-FET430UIF on specified COM port",			// 57
+	"MSP-FET430UIF is already in use",					// 58
+	"Enhanced Emulation Module polling thread is already active",		// 59
+
+	"Could not terminate Enhanced Emulation Module polling thread",		// 60
+	"Could not unlock BSL memory segments",					// 61
+	"Could not perform access, BSL memory segments are protected",		// 62
+	"FOUND_OTHER_DEVICE (errcode=63)", /* XXX */				// 63
+	"Wrong Password provided to open JTAG",					// 64
+	"Invalid error number",							// 65
 };
 
 const char *fet_error(int code)
