dlbeer/mspdebug

commit 2aed1b90a48cb794641c2a6ff1308c0fcc8904f8

Simulator: fix 1-byte indirect INC
Signed-off-by: Radu Rendec <radu.rendec@cloudbit.ro>

sim.c
@@ -110,7 +110,7 @@ static int fetch_operand(struct sim_device *dev,
 			return 0;
 		}
 		addr = dev->regs[reg];
-		dev->regs[reg] += 2;
+		dev->regs[reg] += is_byte ? 1 : 2;
 		break;
 	}
 

commit edf625929c33f272dd0e5163bc5422882660e662

Fix immediate-mode addressing with byte instructions.
Immediate mode is implemented using auto-increment register indexing,
but the advance is always 2 for PC-indexed operands.

Bug introduced in 2aed1b90a48cb794641c2a6ff1308c0fcc8904f8.

sim.c
@@ -110,7 +110,7 @@ static int fetch_operand(struct sim_device *dev,
 			return 0;
 		}
 		addr = dev->regs[reg];
-		dev->regs[reg] += is_byte ? 1 : 2;
+		dev->regs[reg] += (is_byte && reg != MSP430_REG_PC) ? 1 : 2;
 		break;
 	}
 