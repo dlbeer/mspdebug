5477cd78d87bb061f54766efdceb988ddcdd6ea5
Daniel Beer <dlbeer@gmail.com>
2012-11-26 12:27:48 +1300

    Version 0.21

--- ui/main.c
+++ ui/main.c
@@ -81,7 +81,7 @@ static const struct device_class *const driver_table[] = {
 };
 
 static const char *version_text =
-"MSPDebug version 0.20 - debugging tool for MSP430 MCUs\n"
+"MSPDebug version 0.21 - debugging tool for MSP430 MCUs\n"
 "Copyright (C) 2009-2012 Daniel Beer <dlbeer@gmail.com>\n"
 "This is free software; see the source for copying conditions.  There is NO\n"
 "warranty; not even for MERCHANTABILITY or FITNESS FOR A PARTICULAR "
