89b5d2d3dae5cd1daa63961581d26ff0c00e81c8
Tamas TEVESZ <ice@extreme.hu>
2013-12-12 10:48:50 +0100

    sys/select.h for select(2)
    Signed-off-by: Tamas TEVESZ <ice@extreme.hu>

--- util/sockets.c
+++ util/sockets.c
@@ -16,6 +16,10 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
+#ifndef __Windows__
+#include <sys/select.h>
+#endif
+
 #include <errno.h>
 #include <stdlib.h>
 #include <string.h>
