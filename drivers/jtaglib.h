55671036a6bba24c1f2d640185dbda34182d1b40
Tamas TEVESZ <ice@extreme.hu>
2013-12-12 12:54:37 +0100

    Re-style drivers/jtaglib.{c,h} to match the rest of the code
    Strictly style changes only.
    
    Signed-off-by: Tamas TEVESZ <ice@extreme.hu>

--- drivers/jtaglib.h
+++ drivers/jtaglib.h
@@ -26,44 +26,81 @@
 #define JTAGLIB_H_
 
 #include <stdint.h>
+
 #include "jtdev.h"
 #include "util.h"
 
-/*===== public symbols ====================================================== */
-
-/* flash erasing modes */
+/* Flash erasing modes */
 #define JTAG_ERASE_MASS 0xA506
 #define JTAG_ERASE_MAIN 0xA504
 #define JTAG_ERASE_SGMT 0xA502
 
-/*===== public functions =====================================================*/
+/* Take target device under JTAG control. */
+unsigned int jtag_init(struct jtdev *p);
 
-unsigned int jtag_init           (struct jtdev *p);
-unsigned int jtag_get_device     (struct jtdev *p);
-unsigned int jtag_chip_id        (struct jtdev *p);
-uint16_t     jtag_read_mem       (struct jtdev *p, unsigned int format,
-				  address_t address);
-void         jtag_read_mem_quick (struct jtdev *p, address_t start_address,
-				  unsigned int word_count, uint16_t *data);
-void         jtag_write_mem      (struct jtdev *p, unsigned int format,
-				  address_t address, uint16_t data);
-void         jtag_write_mem_quick(struct jtdev *p, address_t start_address,
-				  unsigned int word_count,
-				  const uint16_t *data);
-int          jtag_is_fuse_blown  (struct jtdev *p);
-unsigned int jtag_execute_puc    (struct jtdev *p);
-void         jtag_release_device (struct jtdev *p, address_t address);
-int          jtag_verify_mem     (struct jtdev *p, address_t start_address,
-				  unsigned int word_count,
-				  const uint16_t *data);
-int          jtag_erase_check    (struct jtdev *p, address_t start_address,
-				  unsigned int word_count);
-void         jtag_write_flash    (struct jtdev *p, address_t start_address,
-				  unsigned int word_count,
-				  const uint16_t *data);
-void         jtag_erase_flash    (struct jtdev *p, unsigned int erase_mode,
-				  address_t erase_address);
-address_t    jtag_read_reg       (struct jtdev *p, int reg);
-void         jtag_write_reg      (struct jtdev *p, int reg, address_t value);
+unsigned int jtag_get_device(struct jtdev *p);
+
+/* Read the target chip id. */
+unsigned int jtag_chip_id(struct jtdev *p);
+
+/* Reads one byte/word from a given address */
+uint16_t jtag_read_mem(struct jtdev *p,
+		       unsigned int format,
+		       address_t address);
+
+/* Reads an array of words from target memory */
+void jtag_read_mem_quick(struct jtdev *p,
+			 address_t start_address,
+			 unsigned int word_count,
+			 uint16_t *data);
+
+/* Writes one byte/word at a given address */
+void jtag_write_mem(struct jtdev *p,
+		    unsigned int format,
+		    address_t address,
+		    uint16_t data);
+
+/* Writes an array of words into target memory */
+void jtag_write_mem_quick(struct jtdev *p,
+			  address_t start_address,
+			  unsigned int word_count,
+			  const uint16_t *data);
+
+/* This function checks if the JTAG access security fuse is blown */
+int jtag_is_fuse_blown(struct jtdev *p);
+
+/* Execute a Power-Up Clear (PUC) using JTAG CNTRL SIG register */
+unsigned int jtag_execute_puc(struct jtdev *p);
+
+/* Release the target device from JTAG control */
+void jtag_release_device(struct jtdev *p, address_t address);
+
+/* Performs a verification over the given memory range */
+int jtag_verify_mem(struct jtdev *p,
+		    address_t start_address,
+		    unsigned int word_count,
+		    const uint16_t *data);
+
+/* Performs an erase check over the given memory range */
+int jtag_erase_check(struct jtdev *p,
+		     address_t start_address,
+		     unsigned int word_count);
+
+/* Programs/verifies an array of words into a FLASH */
+void jtag_write_flash(struct jtdev *p,
+		      address_t start_address,
+		      unsigned int word_count,
+		      const uint16_t *data);
+
+/* Performs a mass erase or a segment erase of a FLASH module */
+void jtag_erase_flash(struct jtdev *p,
+		      unsigned int erase_mode,
+		      address_t erase_address);
+
+/* Reads a register from the target CPU */
+address_t jtag_read_reg(struct jtdev *p, int reg);
+
+/* Writes a value into a register of the target CPU */
+void jtag_write_reg(struct jtdev *p, int reg, address_t value);
 
 #endif
